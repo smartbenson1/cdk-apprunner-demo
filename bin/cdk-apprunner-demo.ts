79bf3b5b1171a827059f9a5e62460d01f9862c23
smartbenson1 <kuangyehao.aiesecnku@gmail.com>
2022-10-05 00:01:37 +0800

    Update cdk-apprunner-demo.ts

--- bin/cdk-apprunner-demo.ts
+++ bin/cdk-apprunner-demo.ts
@@ -5,6 +5,6 @@ import { CdkApprunnerDemoStack } from '../lib/cdk-apprunner-demo-stack';
 const app = new cdk.App();
 new CdkApprunnerDemoStack(app, 'CdkApprunnerDemoStack', { 
     env: { 
-      account: '409489027555',
-      region: 'ap-northeast-1'
+      account: 'process.env.CDK_DEFAULT_ACCOUNT',
+      region: 'process.env.CDK_DEFAULT_REGION'
   }});
